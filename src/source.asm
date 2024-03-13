109cc7f1da23775d5973c5a17f652e6f3dfc6305
kbui1213 <159034261+kbui1213@users.noreply.github.com>
2024-03-13 19:06:56 +0000

    against

--- src/source.asm
+++ src/source.asm
@@ -1,10 +1,10 @@
-TITLE Assignment-2
-
-; Name: Kevin Bui
+; Title: Assignment-2
+; Author: Kevin Bui
 ; Date: March 13, 2024
 ; ID: 110110133
 ; Description: Assignment 3
 
+; Including necessary libraries
 INCLUDE Irvine32.inc
 INCLUDELIB Irvine32.lib
 
@@ -13,26 +13,25 @@ INCLUDELIB user32.lib
 
 .data
     MAX = 20 ; Maximum size of the vector
-    N BYTE ?
-    UserVector DWORD MAX DUP(?)
+    N BYTE ? ; Variable for the size of the vector
+    UserVector DWORD MAX DUP(?) ; Vector to store user values
 
     exiting BYTE "I am exiting... Thank you Honey... and Get lost...", 0Ah, 0Dh, 0
 
-
-    promptFirst BYTE "what do you want to do now? > ", 0
-
+    ; Prompts for user interaction
+    promptFirst BYTE "what do you want to do now? (Code broke, on first prompt ENTER can continue code rather than numbers usually)> ", 0
     prompt_two BYTE "What is the size N of Vector? > ", 0
     promptThird BYTE "What are the ", 0
     promptFour BYTE " values in Vector? > ", 0
     promptFifth BYTE "Size of Vector is N = ", 0
     promptSixth BYTE "Vector = ", 0
 
-    
     empty BYTE "Stack is empty ", 0Ah, 0Dh, 0
     notEmpty BYTE "Stack not empty", 0Ah, 0Dh, 0
 
     space_text BYTE ", ", 0
 
+    ; Prompts for stack operations
     Prompt_7 BYTE "Vector is ", 0
     Prompt_8 BYTE "before ArrayToStack ", 0Ah, 0Dh, 0
     Prompt_9 BYTE "Stack is ", 0
@@ -48,14 +47,14 @@ INCLUDELIB user32.lib
     prompt_17 BYTE "Stack is ", 0
     prompt_18 BYTE "after StackReverse", 0Ah, 0Dh, 0
 
+    ; Variables for stack operations
     variable1 DWORD ?
     variable2 DWORD ?
 
-
 .code
 
+; Procedure to check if the stack is empty
 StackEmpty PROC
-
     mov EAX, ESP
     ADD EAX, DWORD
     CMP variable1, EAX
@@ -70,13 +69,12 @@ StackEmpty PROC
         Call WriteString
     if_empty:
     ret
-
 StackEmpty ENDP
 
+; Procedure to print values in the vector
 print_values PROC
-    ; 
-    movzx ECX, N ; set loop counter to the length of vector
-    mov EDI, OFFSET UserVector ; EDI point to beginning of the vector
+    movzx ECX, N ; Set loop counter to the length of the vector
+    mov EDI, OFFSET UserVector ; Point EDI to the beginning of the vector
 
     Loop_Print:
         ; Display the current element
@@ -96,6 +94,7 @@ print_values PROC
     ret
 print_values ENDP
 
+; Procedure to print values in the stack
 stack_valuez PROC
     mov EDI, ESP
     movzx ECX, N
@@ -113,12 +112,13 @@ main PROC
     
     mov variable1, ESP
     mainLoop:
+        ; Display main prompt and read user input
         mov EDX, OFFSET promptFirst
         Call WriteString
-
         Call ReadInt
         Call Crlf
 
+        ; Compare user input to determine action
         CMP EAX, -1
         JE Finishing
         CMP EAX, 0
@@ -130,16 +130,13 @@ main PROC
         CMP EAX, 3
         JE StackReverse
 
-
-    ; Start of 0
+    ; Create a vector
     make_start:
         mov EDX, OFFSET prompt_two
         Call WriteString
-
         Call ReadInt
         mov N, AL
         movzx ECX, N
-
         Call Crlf
 
         mov EDX, OFFSET promptThird
@@ -167,8 +164,8 @@ main PROC
         Call Crlf
         Call StackEmpty
         jmp mainLoop
-    ; End of 0
-    ; Start of 1
+
+    ; Vector to Stack operation
     VectorToStack:
         mov EDX, OFFSET Prompt_7
         Call WriteString
@@ -198,8 +195,8 @@ main PROC
         Call Crlf
         Call StackEmpty
         jmp mainLoop
-    ; End of 1
-    ; Start of 2
+
+    ; Stack to Vector operation
     StackToVector:
         mov EDX, OFFSET Prompt_13
         Call WriteString
@@ -226,8 +223,8 @@ main PROC
         Call Crlf
         Call StackEmpty
         jmp mainLoop
-    ; End of 2
-    ; Start of 3
+
+    ; Reverse Stack operation
     StackReverse:
         mov EDX, OFFSET prompt_15
         Call WriteString
@@ -266,8 +263,8 @@ main PROC
         Call Crlf
         Call StackEmpty
         jmp mainLoop
-    ; end of 3 option
-    ; -1 exit option
+
+    ; Exit option
     Finishing:
         mov EDX, OFFSET exiting
         Call WriteString
